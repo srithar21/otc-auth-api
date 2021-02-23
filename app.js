ed5ddacf48e1687c7b5642a6139e16643faa8998
Srithar Rajendran <sritharrajendran@Srithars-Mac-mini.local>
2021-02-22 21:25:14 -0700

    Added the .env and cors script

--- app.js
+++ app.js
@@ -11,9 +11,16 @@ var app = express();
 
 
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  next();
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
