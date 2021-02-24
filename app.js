8b6d72fddf37c07d4fa958838326bf9c994a2659
Srithar Rajendran <sritharrajendran@Srithars-Mac-mini.local>
2021-02-24 14:24:47 -0700

    Folder name modified

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var usersRouter = require('./routes/users');
-var accountRoutes = require('./controller/accounts/routes')
+var usersRouter = require('./src/routes/users');
+var accountRoutes = require('./src/controller/accounts/routes')
 
 var app = express();
 
@@ -32,12 +32,6 @@ var cors = require('cors');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-//   next();
-// });
 
 
 // use it before all route definitions
