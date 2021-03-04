95ea66875336670b7a6a42c19219648a0d37dca9
Srithar Rajendran <sritharrajendran@Srithars-Mac-mini.local>
2021-03-04 00:31:32 -0700

    Modified the account api to /detail

--- src/controller/accounts/routes.js
+++ src/controller/accounts/routes.js
@@ -12,7 +12,7 @@ router.get('/', accountController.root)
 
 router.post('/login', accountController.siginWithPassword)
 
-router.get('/account', accountController.accountInfo)
+router.get('/detail', accountController.accountInfo)
 
 
 module.exports = router
