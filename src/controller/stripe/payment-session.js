34777bd71089c2b77565b0b8498718132635bb2d
Srithar Rajendran <sritharrajendran@Srithars-Mac-mini.local>
2021-03-20 19:47:44 -0600

    Updated the session test key

--- src/controller/stripe/payment-session.js
+++ src/controller/stripe/payment-session.js
@@ -6,7 +6,7 @@ const { ReasonPhrases,
     getStatusCode,} = require("http-status-codes");
 const httpUtils = require('../../util/httputils');
 
-const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+const stripe = require('stripe')('sk_test_51IL9i5FzfcjDT1x8NKOn12BKxKRxttlWBHaAzkDdxjZydQLWglGFQM3NNBkcSCm67NImEn60i1kFaCDp2nNQ8bTE00PFJVtZfp');
 // const YOUR_DOMAIN = 'http://localhost:3000';
 const YOUR_DOMAIN = 'https://auth-qa-service.azurewebsites.net';
 
@@ -14,7 +14,7 @@ const YOUR_DOMAIN = 'https://auth-qa-service.azurewebsites.net';
 exports.createSession = async (req, res) => { 
     try {
         console.log(req.body)
-        console.log(httpUtils.hostURL)
+        console.log(req.body.price)
         // res.send("Sample service")
 
         const session = await stripe.checkout.sessions.create({
@@ -22,17 +22,19 @@ exports.createSession = async (req, res) => {
             line_items: [
               {
                 price_data: {
-                  currency: 'usd',
-                  product_data: {
-                    name: 'Stubborn Attachments',
-                    images: ['https://i.imgur.com/EHyR2nP.png'],
-                  },
-                  unit_amount: 2000,
+                    currency: 'usd',
+                    product_data: {
+                      name: 'Stubborn Attachments',
+                      images: ['https://i.imgur.com/EHyR2nP.png'],
+                    },
+                    unit_amount: 2000,
                 },
-                quantity: 1,
-              },
-            ],
+                  quantity: 1,
+            }],
             mode: 'payment',
+            customer_email:req.body.email,
+            locale:'en',
+
             success_url: `${YOUR_DOMAIN}/success.html`,
             cancel_url: `${YOUR_DOMAIN}/cancel.html`,
           });
