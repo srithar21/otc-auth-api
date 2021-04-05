154d675a45be828abb2af0f7bf65de3affbd754a
gokulakrishnan <gokul@onetimecode.io>
2021-04-05 20:46:59 +0530

    update

--- src/controller/stripe/payment-session-routes.js
+++ src/controller/stripe/payment-session-routes.js
@@ -3,5 +3,5 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/create-checkout-session', paymentSession.createSession)
-router.post('/create-subscription-session', paymentSession.createSession)
+router.post('/create-subscription-session', paymentSession.createSubscriptionSession)
 module.exports = router
